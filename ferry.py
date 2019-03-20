5bd55bd55388f3b0f69bf85d986128d1432b7e9d
Kitutu Paul <pkitutu@gmail.com>
2019-03-20 22:43:54 +0300

    ferry module

---
+++ ferry.py
@@ -0,0 +1,40 @@
+"""
+Author: Kitutu Paul
+March, 2019
+"""
+
+class Ferry(object):
+	"""docstring for Ferry"""
+	vehicle_types = ()
+	__max_vehicles = 0
+	current_vehicles = 0
+
+	def __init__(self):
+		super(Ferry, self).__init__()
+
+	def set_vehicle_types(self, vehicle_types):
+		self.vehicle_types = vehicle_types
+
+	def set_max_vehicles(self, max_vehicles):
+		self.__max_vehicles = max_vehicles
+
+	def get_max_vehicles(self):
+		return self.__max_vehicles
+
+	def park_vehicle(self):
+		if self.space_available():
+			self.current_vehicles += 1
+			print "Vehicle parked"
+		else:
+			print "Vehicle failed to park because of no space"
+
+	def space_available(self):
+		return self.current_vehicles<self.__max_vehicles
+
+
+# f = Ferry()
+# f.park_vehicle()
+# f.park_vehicle()
+# f.park_vehicle()
+# f.park_vehicle()
+# print f.space_available()
